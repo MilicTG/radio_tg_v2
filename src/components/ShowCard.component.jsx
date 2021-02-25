2f00612d72425a230f07126596c2b57dfe0c2e5a
MilicTG <milic.tg@gmail.com>
2021-02-25 15:36:47 +0100

    Getting some problems with cards

--- src/components/ShowCard.component.jsx
+++ src/components/ShowCard.component.jsx
@@ -5,12 +5,17 @@ import zrcaloBck from '../assets/bck_show_zrcalo_card.jpg'
 const ShowCard = props => {
     return (
         <figure className="w-64 h-80 rounded-xl shadow-xl relative overflow-hidden border-0">
-            <img className="w-full h-full rounded-xl " src={zrcaloBck} alt="Image"/>
-            <div className="bottom-0 left-0 w-full frosted absolute rounded-b-xl transform translate-y-2/3 group-hover:translate-x-0">
-                <h4 className="text-xl text-gray-800 p-4">Strunica, sarenica...</h4>
-                <p className="text-gray-800 text-s font-light p-4 ">Središnja emisija u kojoj obrađujemo
-                    aktualne teme i
-                    događaje, kako u našoj sredini, tako i šire.</p>
+            <img className="w-full h-full " src={zrcaloBck} alt="Image"/>
+            <div
+                className="bottom-0 left-0 w-full frosted overflow-hidden absolute transform translate-y-2/3 hover:translate-y-0 transition duration-100 ease-in-out">
+                <h5 className="text-lg text-gray-800 py-3 px-4">Strunica, sarenica...</h5>
+                <p className="text-gray-800 text-s font-light p-4 overflow-hidden">uto - pet 11:00</p>
+                <p className="text-gray-800 text-s font-light p-4 overflow-hidden">
+                    Središnja emisija u kojoj obrađujemo
+                    aktualne teme i događaje,
+                    kako u našoj sredini,
+                    tako i šire.</p>
+                <button className="bg-none text-blue-800 p-4">Idi na emisiju -></button>
             </div>
         </figure>
     )
