c869ca8c527d26c30455c5bd4ca63dfc5b431ba5
MilicTG <milic.tg@gmail.com>
2021-03-05 21:30:50 +0100

    Finished card Ui and shows data

--- src/components/ShowCard.component.jsx
+++ src/components/ShowCard.component.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import {motion, AnimatePresence} from "framer-motion"
 
-import zrcaloBck from '../assets/zrcaloShow.jpg'
-
 const ShowCard = props => {
     return (
         <AnimatePresence>
@@ -13,15 +11,12 @@ const ShowCard = props => {
                 }}
                 whileHover={{scale: 1.010}}
                 whileTap={{scale: 0.95}}
-                className="bg-gray-100 p-0 rounded-xl shadow-2xl overflow-hidden border-0 cursor-pointer">
-                <img className="w-full h-40 object-cover content-center" src={zrcaloBck} alt="Show image"/>
-                <div
-                    className="    ">
-                    <h5 className="text-xl text-gray-800 p-4 font-bold">Strunica, šarenica, tkanica</h5>
-                    <p className="p-4">Središnja emisija u kojoj obrađujemo aktualne teme i događaje, kako u našoj
-                        sredini, tako i
-                        šire.</p>
-                    <p className="font-medium bg-gradient-to-r from-blue-800 to-blue-400 text-white p-4 text-sm">Od utorka do petka u 11:00 sati</p>
+                className="place-content-stretch	 bg-gray-100 p-0 rounded-xl shadow-2xl overflow-hidden border-0 cursor-pointer">
+                <img className="w-full h-40 object-cover content-center mb-2" src={props.image} alt="Show image"/>
+                <div className="h-4/6 flex flex-col items-stretch">
+                    <h5 className="text-xl text-gray-800 p-4 font-bold">{props.title}</h5>
+                    <p className="h-40 p-4">{props.desc}</p>
+                    <p className="flex-grow font-light text-blue-800 p-4 text-sm">{props.airTime}</p>
                 </div>
             </motion.figure>
         </AnimatePresence>
--- src/components/Shows.component.jsx
+++ src/components/Shows.component.jsx
@@ -3,21 +3,58 @@ import React from "react";
 import SectionTitle from "./SectionTitle.component";
 import ShowCard from "./ShowCard.component";
 
+import {showCardData} from '../data/local/ShowCardData.js'
+import zrcaloImg from '../assets/img_zrcalo.jpg'
+import strunicaImg from "../assets/img_strunica.jpg"
+import petmilImg from '../assets/img_petmil.jpg'
+import razgovoriImg from '../assets/img_razgovori.jpg'
+import glazbaonicaImg from '../assets/img_glazbaonica.jpg'
+import obiteljskiImg from '../assets/img_obiteljski.jpg'
+import slusamImg from '../assets/img_slusam.jpg'
+import knjiznicaImg from '../assets/img_knjiznica.jpg'
+import ostaloImg from '../assets/img_ostalo.jpg'
+
 const Shows = () => {
     return (
         <section className="w-full flex justify-center mt-20 mb-16">
             <div className="w-full max-w-con h-full flex items-start flex-col justify-center">
-               <SectionTitle title="Naše najslušanije emisije"/>
-                <div className="w-full pt-2 pb-12 grid grid-cols-3 gap-10">
-                    <ShowCard/>
-                    <ShowCard/>
-                    <ShowCard/>
-                    <ShowCard/>
-                    <ShowCard/>
-                    <ShowCard/>
-                    <ShowCard/>
-                    <ShowCard/>
-                    <ShowCard/>
+                <SectionTitle title="Naše najslušanije emisije"/>
+                <div className="w-full pt-2 pb-12 grid grid-cols-3 gap-12">
+                    <ShowCard key={showCardData[0].showId} title={showCardData[0].showTitle}
+                              image={zrcaloImg}
+                              desc={showCardData[0].showDescription} airTime={showCardData[0].showAirTime}/>
+
+                    <ShowCard key={showCardData[1].showId} title={showCardData[1].showTitle}
+                              image={strunicaImg}
+                              desc={showCardData[1].showDescription} airTime={showCardData[1].showAirTime}/>
+
+                    <ShowCard key={showCardData[2].showId} title={showCardData[2].showTitle}
+                              image={petmilImg}
+                              desc={showCardData[2].showDescription} airTime={showCardData[2].showAirTime}/>
+
+                    <ShowCard key={showCardData[3].showId} title={showCardData[3].showTitle}
+                              image={glazbaonicaImg}
+                              desc={showCardData[3].showDescription} airTime={showCardData[3].showAirTime}/>
+
+                    <ShowCard key={showCardData[4].showId} title={showCardData[4].showTitle}
+                              image={razgovoriImg}
+                              desc={showCardData[4].showDescription} airTime={showCardData[4].showAirTime}/>
+
+                    <ShowCard key={showCardData[5].showId} title={showCardData[5].showTitle}
+                              image={obiteljskiImg}
+                              desc={showCardData[5].showDescription} airTime={showCardData[5].showAirTime}/>
+
+                    <ShowCard key={showCardData[6].showId} title={showCardData[6].showTitle}
+                              image={slusamImg}
+                              desc={showCardData[6].showDescription} airTime={showCardData[6].showAirTime}/>
+
+                    <ShowCard key={showCardData[7].showId} title={showCardData[7].showTitle}
+                              image={knjiznicaImg}
+                              desc={showCardData[7].showDescription} airTime={showCardData[7].showAirTime}/>
+
+                    <ShowCard key={showCardData[8].showId} title={showCardData[8].showTitle}
+                              image={ostaloImg}
+                              desc={showCardData[8].showDescription} airTime={showCardData[8].showAirTime}/>
                 </div>
             </div>
         </section>
