1bc246c28182343fbca5594fb7cdf26cd51abd47
MilicTG <milic.tg@gmail.com>
2021-03-19 21:26:03 +0100

    finished with footer

--- src/components/Footer.component.jsx
+++ src/components/Footer.component.jsx
@@ -1,75 +1,64 @@
-import React from 'react'
+import React from "react";
 
-import {FaPhoneAlt, FaFax, FiMail, ImLocation2} from 'react-icons/all'
-
-import logoLight from '../assets/logo_rtg_main_white.svg'
+import logoLight from "../assets/logo_rtg_main_white.svg";
 
 const Footer = () => {
-    return (
-        <footer className="w-full h-auto bg-gray-900 flex justify-center">
-            <div className="max-w-con w-full flex flex-col items-center justify-center py-16">
+  return (
+    <footer className="w-full h-auto px-4 xl:px-0 bg-gray-900 flex justify-center">
+      <div className="max-w-con w-full flex flex-col items-center justify-center py-8 xl:py-16">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-between items-start">
+          <div className=" md:col-span-2 lg:col-span-1">
+            <img src={logoLight} alt="" className="w-40 h-auto py-4" />
+            <p className="text-white">Trg dr. Franje Tuđmana bb</p>
+            <p className="text-white">80240, Tomislavgrad, BiH</p>
+          </div>
 
-                <img src={logoLight} alt="" className="w-40 h-auto mb-16"/>
-
-                <div className="w-full flex flex-row justify-between items-start">
-
-                    <div className="flex flex-row">
-                        <div>
-                            <FaPhoneAlt size="2rem" className="text-white"/>
-                        </div>
-                        <div className="px-4">
-                            <h5 className="text-2xl text-white pb-4">Telefon</h5>
-                            <p className="text-white">
-                                <span className="font-bold">Redakcija:</span> +387 34 352 909</p>
-                            <p className="text-white"><span className="font-bold">Marketing:</span> +387
-                                34 353 768</p>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-row">
-                        <div className="">
-                            <FaFax size="2rem" className="text-white"/>
-                        </div>
-                        <div className="px-4">
-                            <h5 className="text-2xl text-white pb-4">Fax</h5>
-                            <p className="text-white">+387 34 352 909</p>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-row">
-                        <div className="">
-                            <FiMail size="2rem" className="text-white"/>
-                        </div>
-                        <div className="px-4">
-                            <h5 className="text-2xl text-white pb-4">E-Mail</h5>
-                            <p className="text-white">radiotomislavgrad@gmail.com</p>
-                            <p className="text-white">radio.tomislavgrad@tel.net.ba</p>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-row">
-                        <div className="">
-                            <ImLocation2 size="2rem" className="text-white"/>
-                        </div>
-                        <div className="px-4">
-                            <h5 className="text-2xl text-white pb-4">Adresa</h5>
-                            <p className="text-white">Trg dr. Franje Tuđmana bb</p>
-                            <p className="text-white">80240, Tomislavgrad, BiH</p>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="w-full h-0.5 bg-blue-200 rounded-3xl mt-8 mb-2"/>
-
-                <div className="w-full h-auto py-1 flex flex-row justify-between">
-                    <p className="text-xs text-blue-50">Sva prava pridrzana Radio Tomislavgrad</p>
-                    <p className="text-xs text-blue-50">Designed and developed by milic.dev</p>
-                </div>
-
+          <div className="flex flex-row py-8">
+            <div className="px-0 xl:px-4">
+              <h6 className="text-xl text-white opacity-60 pb-4">Telefon</h6>
+              <p className="text-white">
+                <span className="font-bold">Redakcija:</span> +387 34 352 909
+              </p>
+              <p className="text-white">
+                <span className="font-bold">Marketing:</span> +387 34 353 768
+              </p>
             </div>
-        </footer>
-    )
-}
+          </div>
 
-export default Footer
+          <div className="flex flex-row py-8">
+            <div className="px-0 xl:px-4">
+              <h6 className="text-xl text-white opacity-60 pb-4">E-Mail</h6>
+              <p className="text-white">radiotomislavgrad@gmail.com</p>
+              <p className="text-white">radio.tomislavgrad@tel.net.ba</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row py-8">
+            <div className="px-0 xl:px-4">
+              <h6 className="text-xl text-white opacity-60 pb-4">
+                Pls za stream
+              </h6>
+              <p className="text-white">Radio Tomislavgrad</p>
+              <p className="text-white">RTG Music</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full">
+          <div className="w-full h-0.5 bg-blue-200 rounded-3xl mt-8 mb-2" />
+
+          <div className="w-full h-auto py-1 flex flex-col lg:flex-row justify-between">
+            <p className="text-xs text-blue-50">
+              Sva prava pridrzana Radio Tomislavgrad
+            </p>
+            <p className="text-xs text-blue-50">
+              Designed and developed by milic.dev
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/InfoSection.component.jsx
+++ src/components/InfoSection.component.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import SectionTitle from "./SectionTitle.component";
 
 const InfoSection = (props) => {
   return (
     <div className="w-full pt-12 pb-12 lg:pt-16 lg:pb-16 flex justify-center">
-      <div className="max-w-con h-full flex flex-col px-4 lg:px-0 lg:flex-row lg:items-center justify-around">
+      <motion.div
+        initial={{ x: -100 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 1 }}
+        className="max-w-con h-full flex flex-col px-4 lg:px-0 lg:flex-row lg:items-center justify-around"
+      >
         <div
           className={
             props.order
@@ -18,12 +24,15 @@ const InfoSection = (props) => {
           <p className="mb-5 text-gray-900">{props.textTwo}</p>
           <p className="mb-5 text-gray-900">{props.textThree}</p>
         </div>
-        <img
+        <motion.img
+          initial={{ opacity: 0, scale: 0.2 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1 }}
           src={props.image}
           className="h-72 lg:h-96 rounded-md shadow-2xl object-cover"
           alt=""
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
