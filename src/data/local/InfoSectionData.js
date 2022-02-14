efe3a9895a3c55faaeca52e0c060c64e00a8333f
Ivan Milic <milic.tg@gmail.com>
2022-02-14 08:31:55 +0100

    Camera Update

--- src/data/local/InfoSectionData.js
+++ src/data/local/InfoSectionData.js
@@ -1,27 +1,36 @@
 export const aboutUsData = {
-    title: "O Nama",
-    textOne: "Radio Tomislavgrad osnovan je 18. svibnja 1992. godine, u vrijeme najžešćih ratnih operacija na kupreško – duvanjskoj bojišnici, što znači da je utemeljen radi potrebe informiranja stanovništva u tim ratnim i kaotičnim prilikama. Od početnih nekoliko sati, od 18. prosinca 1998. emitiramo 24 sata vlastitoga programa.",
-    textTwo: "Emitiramo s tri odašiljača, na frekvencijama: 103.3, 95.9 i 95.1 MHZ. Zahvaljujući internetu imamo određeni broj slušatelja u cijelome svijetu. Uglavnom su to naši ljudi koji žele čuti informacije iz rodnoga kraja, a slušaju nas na adresi www.radiotg.com",
-    textThree: "Kroz sve ove godine našega djelovanja uvijek smo nastojali biti u službi našim slušateljima. Tako je i danas. Zato, pronađite naš signal i uvjerite se u kvalitetu i raznolikost programa koji svakodnevno nudimo. Vaš RTG"
-}
+  title: "O Nama",
+  textOne:
+    "Radio Tomislavgrad osnovan je 18. svibnja 1992. godine, u vrijeme najžešćih ratnih operacija na kupreško – duvanjskoj bojišnici, što znači da je utemeljen radi potrebe informiranja stanovništva u tim ratnim i kaotičnim prilikama. Od početnih nekoliko sati, od 18. prosinca 1998. emitiramo 24 sata vlastitoga programa.",
+  textTwo:
+    "Emitiramo s tri odašiljača, na frekvencijama: 103.3, 95.9 i 95.1 MHZ. Zahvaljujući internetu imamo određeni broj slušatelja u cijelome svijetu. Uglavnom su to naši ljudi koji žele čuti informacije iz rodnoga kraja, a slušaju nas na adresi www.radiotg.com",
+  textThree:
+    "Kroz sve ove godine našega djelovanja uvijek smo nastojali biti u službi našim slušateljima. Tako je i danas. Zato, pronađite naš signal i uvjerite se u kvalitetu i raznolikost programa koji svakodnevno nudimo. Vaš RTG",
+};
 
 export const marketYourBusinessData = {
-    title: "Reklamirajte svoj posao",
-    textOne: 'Obično se kaže da, uz kvalitetu, proizvod prodaje i dobra reklama. To je općepoznata činjenica u poslovnom svijetu.Kreativnost i radiofoničnost glasova ljudi pred mikrofonom i osposobljenost naših tehničkih urednika, jamče dobar i uspješan marketinški proizvod.',
-    textTwo: 'Kreativnost i radiofoničnost glasova ljudi pred mikrofonom i osposobljenost naših tehničkih urednika, jamče dobar i uspješan marketinški proizvod.',
-    textThree: 'Kontaktirajte nas na telefon: +387 34 353 768'
-}
+  title: "Reklamirajte svoj posao",
+  textOne:
+    "Obično se kaže da, uz kvalitetu, proizvod prodaje i dobra reklama. To je općepoznata činjenica u poslovnom svijetu.",
+  textTwo:
+    "Kreativnost i radiofoničnost glasova ljudi pred mikrofonom i osposobljenost naših tehničkih urednika, jamče dobar i uspješan marketinški proizvod.",
+  textThree: "Kontaktirajte nas na telefon: +387 34 353 768",
+};
 
 export const weddingAndOtherData = {
-    title: "Prigodne čestitke",
-    textOne: "Lijepo je usrećiti nekoga u našem svakodnevnom životu.",
-    textTwo: "Vjenčanja, godišnjice braka, rođendani, imendani, prigoda su da na neki način iznenadimo i razveselimo drage ljude. A najbolji način za to je uputiti im čestitku s najljepšim željama i prigodnim pjesmama putem valova Radija Tomislavgrad.",
-    textThree: "Učinite to, nazovite nas, a ostalo je naša briga."
-}
+  title: "Prigodne čestitke",
+  textOne: "Lijepo je usrećiti nekoga u našem svakodnevnom životu.",
+  textTwo:
+    "Vjenčanja, godišnjice braka, rođendani, imendani, prigoda su da na neki način iznenadimo i razveselimo drage ljude. A najbolji način za to je uputiti im čestitku s najljepšim željama i prigodnim pjesmama putem valova Radija Tomislavgrad.",
+  textThree: "Učinite to, nazovite nas, a ostalo je naša briga.",
+};
 
 export const androidAppData = {
-    title: "Android aplikacija",
-    textOne: "Nam et porta enim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget est commodo, mollis massa eget, dignissim est. Suspendisse eu sapien id metus pulvinar porttitor.",
-    textTwo: "Nam imperdiet magna placerat elit malesuada, eget luctus velit ullamcorper. Aliquam laoreet enim at dapibus porta. Donec accumsan dui nec odio sodales, eu fringilla urna tristique. Ut feugiat sapien eu imperdiet lobortis. In pharetra ac turpis eget interdum.",
-    textThree: " Aliquam eget rutrum sem. Integer finibus aliquet tortor, posuere lobortis leo tincidunt sit amet. Morbi dignissim est justo, quis sagittis magna sollicitudin id. Proin rhoncus mollis lorem, id eleifend libero placerat ac. Sed porta ipsum in fringilla convallis."
-}
+  title: "Android aplikacija",
+  textOne:
+    "Nam et porta enim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget est commodo, mollis massa eget, dignissim est. Suspendisse eu sapien id metus pulvinar porttitor.",
+  textTwo:
+    "Nam imperdiet magna placerat elit malesuada, eget luctus velit ullamcorper. Aliquam laoreet enim at dapibus porta. Donec accumsan dui nec odio sodales, eu fringilla urna tristique. Ut feugiat sapien eu imperdiet lobortis. In pharetra ac turpis eget interdum.",
+  textThree:
+    " Aliquam eget rutrum sem. Integer finibus aliquet tortor, posuere lobortis leo tincidunt sit amet. Morbi dignissim est justo, quis sagittis magna sollicitudin id. Proin rhoncus mollis lorem, id eleifend libero placerat ac. Sed porta ipsum in fringilla convallis.",
+};
