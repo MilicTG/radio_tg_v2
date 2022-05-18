6826025e36ebdfef8d81d8dbd8a6a76c277a12be
Ivan Milic <milic.tg@gmail.com>
2022-05-18 10:24:47 +0200

    Update StreamRadioUrls.js

--- src/data/local/StreamRadioUrls.js
+++ src/data/local/StreamRadioUrls.js
@@ -1,6 +1,6 @@
-export const RtgMainStream = "http://163.172.47.181:8038/;";
-export const RtgMusicStream = "http://163.172.47.181:8197/stream";
-export const RtgBastinaStream = "http://163.172.47.181:8139/stream";
-export const RtgFramaStream = "http://163.172.47.181:8143/stream";
-export const RtgDjecjiStream = "http://163.172.47.181:8147/stream";
-export const RtgDomovinaStream = "http://163.172.47.181:8165/stream";
+export const RtgMainStream = "http://158.69.119.6:8038/;";
+export const RtgMusicStream = "http://158.69.119.6:8197//stream";
+export const RtgBastinaStream = "http://158.69.119.6:8139/stream";
+export const RtgFramaStream = "http://158.69.119.6:8143/stream";
+export const RtgDjecjiStream = "http://158.69.119.6:8147//stream";
+export const RtgDomovinaStream = "http://158.69.119.6:8165/stream";
