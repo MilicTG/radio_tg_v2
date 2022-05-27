b9ed85a80ac266e8a606674663253b7b837541d6
MilicTG <milic.tg@gmail.com>
2022-05-27 22:53:59 +0200

    Update SectionLiveCamera.comonent.jsx

--- src/components/SectionLiveCamera.comonent.jsx
+++ src/components/SectionLiveCamera.comonent.jsx
@@ -14,7 +14,7 @@ const SectionLiveCamera = () => {
         <div className="w-full h-auto pb-28">
           <iframe
             className="w-full sm:h-72 md:h-80 lg:h-96 xl:h-screen 2xl:h-screen"
-            src="https://www.youtube.com/embed/bnWRZW9W1zc"
+            src="https://www.youtube.com/embed/fdBdTX1eGjw"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
